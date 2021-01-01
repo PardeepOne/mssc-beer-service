90ed9ee81cf1ab9d379f6c4f57741299ea6c0b8c
Pardeep <pardeeps460@gmail.com>
2021-01-01 17:02:13 +0100

    initializing data via data.sql file closes #5

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * Created by jt on 2019-05-17.
@@ -16,6 +17,9 @@ public class BeerLoader implements CommandLineRunner {
     public static final String BEER_1_UPC = "454654646";
     public static final String BEER_2_UPC = "455435324";
     public static final String BEER_3_UPC = "323123332";
+    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
+    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
+    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
     private final BeerRepository beerRepository;
 
     public BeerLoader(BeerRepository beerRepository) {
@@ -24,37 +28,6 @@ public class BeerLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        loadBeerObjects();
-    }
-
-    private void loadBeerObjects() {
-        if (beerRepository.count() == 0) {
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .build());
-
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-            beerRepository.save(Beer.builder()
-                    .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .quantityToBrew(200)
-                    .minOnHand(12)
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .build());
-        }
+        //using data.sql to initialize database now
     }
 }
